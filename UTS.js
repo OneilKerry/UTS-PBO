5e6d6d20d18d1068ff9f008d78030987495ffdc5
unknown <oneilkerrylaurent1905@gmail.com>
2025-11-06 09:50:37 +0800

    first commit

---
+++ UTS.js
@@ -0,0 +1,67 @@
+class Cuti {
+  constructor(nama) {
+    this.nama = nama;
+  }
+
+  maxHari() {
+    return 12;
+  }
+
+
+  ajukanCuti(name, jumlahHari) {
+     if (this.maxHari() < jumlahHari ) {
+         console.log(`Pengajuan atas nama ${name} ${this.nama} selama ${jumlahHari} hari disetujui`);
+        } else {
+            console.log(`Pengajuan atas nama ${name} ditolak! Maksimal ${this.nama} adalah ${this.maxHari()} hari`);
+        }
+  }
+}
+
+class CutiTahunan extends Cuti {
+  constructor(name) {
+    super(name = "Cuti Tahunan");
+  }
+  maxHari() {
+    return 12;
+  }
+}
+
+class CutiSakit extends Cuti {
+  constructor(name) {
+    super(name = "Cuti Sakit");
+  }
+  maxHari() {
+    return 2;
+  }
+}
+
+class CutiMelahirkan extends Cuti {
+  constructor(name) {
+    super(name= "Cuti Melahirkan");
+  }
+  maxHari() {
+    return 90;
+  }
+}
+//yg bener
+const karywan = new CutiTahunan();
+karywan.ajukanCuti("budi", 13);  
+
+const karywan1 = new CutiSakit();
+karywan1.ajukanCuti("Andi",2); 
+
+const karywan2 = new CutiMelahirkan();
+karywan2.ajukanCuti("Andi",45); 
+
+//yg salah
+const karywan3 = new CutiTahunan();
+karywan.ajukanCuti("Jaky",13);  
+
+const karywan4 = new CutiSakit();
+karywan1.ajukanCuti(4); 
+
+const karywan5 = new CutiMelahirkan();
+karywan2.ajukanCuti(95); 
+
+
+
